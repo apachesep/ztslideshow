38c0fa67fe86e5e343693a68ac24179226d80116
Viet Vu <jooservices@gmail.com>
2015-04-07 09:47:42 +0700

    ^ update with youtube embed

--- libraries/embed.php
+++ libraries/embed.php
@@ -14,12 +14,35 @@ if (!class_exists('ZtSlideshowEmbed'))
 
         private function _prepare()
         {
-            
+            $uri = JUri::getInstance($this->get('embed'));
+            $host = $uri->getHost();
+            if ($host == 'www.youtube.com' || $host == 'youtube.com')
+            {
+                $id = $uri->getVar('v');
+                $this->set('source', 'youtube');
+                $this->set('id', $id);
+            } else
+            {
+                // Vimeo
+            }
         }
 
-        public function getEmbedCode()
+        public function getEmbed($attributes = array())
         {
-            
+
+            foreach ($attributes as $key => $value)
+            {
+                $htmlAttributes [] = $key . '="' . $value . '"';
+            }
+            if ($this->get('source') == 'youtube')
+            {
+                $htmlAttributes [] = 'width="560"';
+                $htmlAttributes [] = 'height="315"';
+                $htmlAttributes [] = 'src="//www.youtube.com/embed/w1jIyPWF9No' . $this->get('id') . '"';
+                $html = '<iframe ' . implode(' ', $htmlAttributes) . ' frameborder="0" allowfullscreen></iframe>';
+            }
+
+            return $html;
         }
 
     }
