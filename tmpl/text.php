b488d7ccafe0f85f3fd33328aa3091dc45ecccac
Viet Vu <jooservices@gmail.com>
2015-04-16 12:15:57 +0700

    ^ improve check field text before use it

--- tmpl/text.php
+++ tmpl/text.php
@@ -3,15 +3,26 @@
 ?>
 
 <div class="content-text ps-<?php echo ($item->get('position')) ? $item->get('position') : 'none'; ?>">
-    <p class="small-title animated <?php echo ($item->get('effect')) ? $item->get('effect') : ''; ?>"
-       style="animation-delay: 0.2s; -webkit-animation-delay: 0.2s;"><?php echo ($item->get('text-title')) ? $item->get('text-title') : ''; ?></p>
-
-    <h3 class="title animated <?php echo ($item->get('effect')) ? $item->get('effect') : ''; ?>"
-        style="animation-delay: 0.5s; -webkit-animation-delay: 0.5s;"><?php echo ($item->get('text-stitle')) ? $item->get('text-stitle') : ''; ?></h3>
-
-    <p class="description animated <?php echo ($item->get('effect')) ? $item->get('effect') : ''; ?>"
-       style="animation-delay: 0.8s; -webkit-animation-delay: 0.8s;"><?php echo ($item->get('text-des')) ? $item->get('text-des') : ''; ?></p>
-
-    <p class="zt-readmore animated <?php echo ($item->get('effect')) ? $item->get('effect') : ''; ?>"
-       style="animation-delay: 1s; -webkit-animation-delay: 1s;"><a href="<?php echo ($item->get('link')) ? $item->get('link') : ''; ?>"><?php echo ($item->get('text-link')) ? $item->get('text-link') : ''; ?></a></p>
+    <?php if (!empty($item->get('text-title'))) : ?>
+        <p class="small-title animated <?php echo ($item->get('effect')) ? $item->get('effect') : ''; ?>"
+           style="animation-delay: 0.2s; -webkit-animation-delay: 0.2s;">
+               <?php echo ($item->get('text-title')) ? $item->get('text-title') : ''; ?>
+        </p>
+    <?php endif; ?>
+    <?php if (!empty($item->get('text-stitle'))) : ?>
+        <h3 class="title animated <?php echo ($item->get('effect')) ? $item->get('effect') : ''; ?>"
+            style="animation-delay: 0.5s; -webkit-animation-delay: 0.5s;">
+            <?php echo ($item->get('text-stitle')) ? $item->get('text-stitle') : ''; ?></h3>
+    <?php endif; ?>
+    <?php if (!empty($item->get('text-des'))) : ?>
+        <p class="description animated <?php echo ($item->get('effect')) ? $item->get('effect') : ''; ?>"
+           style="animation-delay: 0.8s; -webkit-animation-delay: 0.8s;">
+            <?php echo ($item->get('text-des')) ? $item->get('text-des') : ''; ?></p>
+    <?php endif; ?>
+    <?php if (!empty($item->get('link')) && !empty($item->get('text-link'))) : ?>
+        <p class="zt-readmore animated <?php echo ($item->get('effect')) ? $item->get('effect') : ''; ?>"
+           style="animation-delay: 1s; -webkit-animation-delay: 1s;">
+            <a href="<?php echo ($item->get('link')) ? $item->get('link') : ''; ?>"><?php echo ($item->get('text-link')) ? $item->get('text-link') : ''; ?></a>
+        </p>
+    <?php endif; ?>
 </div>
